80fa2432bb1baaace24e289f29e48b0f630a15b9
RochaCodes <up202305891@g.uporto.pt>
2024-10-16 19:55:07 +0100

    14, collisionsss are now registed

--- src/main/java/com/rocha/hero7/Position.java
+++ src/main/java/com/rocha/hero7/Position.java
@@ -1,5 +1,7 @@
 package com.rocha.hero7;
 
+import java.util.Objects;
+
 public class Position {
     private int x;
     private int y;
@@ -19,4 +21,16 @@ public class Position {
     public void setY(int y) {
         this.y = y;
     }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.getX() && y == position.getY();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
 }
