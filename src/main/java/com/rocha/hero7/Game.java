37b036de09c8e1e35d2f77c5bdcd8b882bfad993
RochaCodes <up202305891@g.uporto.pt>
2024-10-16 15:46:15 +0100

    Ponto 8, operacional

--- src/main/java/com/rocha/hero7/Game.java
+++ src/main/java/com/rocha/hero7/Game.java
@@ -5,13 +5,18 @@ import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 
 import java.io.IOException;
 
 public class Game {
+    private int x = 10;
+    private int y = 10;
     private Screen screen;
+
 
     public Game() {
         try {
@@ -27,13 +32,44 @@ public class Game {
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowUp:
+                y = Math.max(0, y - 1);
+                break;
+            case ArrowDown:
+                y = Math.min(screen.getTerminalSize().getRows() - 1, y + 1);
+                break;
+            case ArrowLeft:
+                x = Math.max(0, x - 1);
+                break;
+            case ArrowRight:
+                x = Math.min(screen.getTerminalSize().getColumns() - 1, x + 1);
+                break;
+            default:
+                break;
+        }
+
+    }
+
 
     public void run() {
         try {
-            draw();
+            while (true) {
+                draw();
+                KeyStroke key = screen.readInput();
+                processKey(key);
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+                    screen.close();
+                    break;
+                }
+                if (key.getKeyType() == KeyType.EOF) {
+                    break;
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
