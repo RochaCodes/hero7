343cad1c757719f21aafc2693740796bbeba9e8c
RochaCodes <up202305891@g.uporto.pt>
2024-10-16 14:46:30 +0100

    creating game 7

---
+++ src/main/java/com/rocha/hero7/Application.java
@@ -0,0 +1,16 @@
+package com.rocha.hero7;
+
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) {
+        Game game = new Game();
+        try {
+            game.run();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
